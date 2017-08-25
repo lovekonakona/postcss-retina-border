65187ea616e0f702bae363a27b18ad026d8e4340
konakona <lovekonakona@gmail.com>
2017-08-25 15:45:21 +0800

    Add unit test support

---
+++ index.js
@@ -0,0 +1,12 @@
+var postcss = require('postcss');
+var process = require('./lib/process');
+
+module.exports = postcss.plugin('postcss-retina-border', function(options) {
+  return function(css) {
+    options = options || {};
+
+    css.walkRules((rule) => {
+      process(rule, rule.nodes);
+    });
+  }
+});
---
+++ index.test.js
@@ -0,0 +1,21 @@
+var postcss = require('postcss');
+var plugin = require('./');
+var test = require('ava');
+
+function run(t, input, output, opts = {}) {
+  return postcss([ plugin(opts) ])
+    .process(input)
+    .then( result => {
+      t.is(result.css, output);
+      t.is(result.warnings().length, 0);
+    });
+}
+
+// This test passed.
+test('Remove border-width', t => {
+  return run(
+    t,
+    'a{ border-width: 1px solid #ccc; }',
+    'a{ }'
+  );
+});
