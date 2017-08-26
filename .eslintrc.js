81d1454ff03fcd12708a21076d969f48832b7688
konakona <lovekonakona@gmail.com>
2017-08-26 11:05:23 +0800

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,6 @@ module.exports = {
     "extends": "standard",
     "rules": {
         "semi": [2, 'always'],
-        "space-before-function-paren": [0, 'always']
+        "space-before-function-paren": [2, 'never']
     }
 };
